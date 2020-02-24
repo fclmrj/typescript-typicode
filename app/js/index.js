8c3d0cf49ab8762360a398402c219c279ad2b031
Fabio Moura <fclmrj@msn.com>
2020-02-24 14:26:38 -0300

    commit initial project

---
+++ app/js/index.js
@@ -0,0 +1,9 @@
+const divPosts = document.querySelector('[posts]');
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(elements => elements.map((obj) => obj)
+    .filter((element) => element.userId == 9)
+    .forEach((element) => {
+    console.log(element.userId + ' - ' + element.id + ' - ' + element.title);
+    divPosts.innerHTML += `<a href="#" class="list-group-item list-group-item-action">${element.title}</a>`;
+}));
---
+++ app/ts/index.ts
@@ -0,0 +1,13 @@
+import {Typicode} from 'typicode';
+
+const divPosts = document.querySelector('[posts]');
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(elements => elements.map((obj: Typicode) => obj)
+    .filter((element : Typicode) => element.userId == 9)
+    .forEach((element : Typicode) => {
+      console.log(element.userId + ' - ' + element.id + ' - ' + element.title);
+      divPosts.innerHTML += `<a href="#" class="list-group-item list-group-item-action">${element.title}</a>`;
+    })
+  );
